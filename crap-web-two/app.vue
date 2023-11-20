e4689a97dc1c1f110709fb18050ed272af8adcf3
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2023-11-19 16:22:23 -0800

    Update app.vue

--- crap-web-two/app.vue
+++ crap-web-two/app.vue
@@ -1,5 +1,144 @@
+<script setup lang="ts">
+import {
+  Badge,
+  Button,
+  ReportModal,
+  ProjectCard,
+  ScaleIcon,
+  MarkdownEditor,
+} from "omorphia";
+import "omorphia/dist/style.css";
+import { useCompactNumber } from "./composables/compact-number";
+
+const isTextVisible = ref(false);
+
+//markdownEditor
+import { ref } from "vue";
+const description = ref(null);
+
+function onButtonPress() {
+  isTextVisible.value = true;
+}
+
+const reportModal = ref<typeof ReportModal | null>(null);
+
+const { data } = await useModrinthData("project/{id|slug}", {
+  pathParams: {
+    "id|slug": "sodium",
+  },
+});
+
+const { data: members } = await useModrinthData("project/{id|slug}/members", {
+  pathParams: {
+    "id|slug": "sodium",
+  },
+});
+
+const sortedMembers = [...members.value].sort(
+  (memberA, memberB) => (memberA.ordering ?? 0) - (memberB.ordering ?? 0)
+);
+
+const formatCompactNumber = useCompactNumber();
+
+function onPrisonSend() {
+  alert("sent to prison");
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <Badge type="admin"></Badge>
+    <Button large color="green" @click="onButtonPress">Click me</Button>
+    <template v-if="isTextVisible">Hehe you pressed the button</template> -->
+
+    <Button @click="reportModal?.show()">Show Modal</Button>
+    <ReportModal
+      ref="reportModal"
+      itemType="project"
+      :reportTypes="[
+        'Stolen from nineMinecraft',
+        'Created by Curseforge User',
+        'notgamer',
+        'windowsuser',
+      ]"
+    />
+
+    {{ String(formatCompactNumber(data.downloads)) }}
+
+    <div v-if="data" class="project-list display-mode--list">
+      <ProjectCard
+        :id="data.id"
+        :type="data.project_type"
+        :name="data.title"
+        :author="sortedMembers[0].user.username"
+        :iconUrl="data.icon_url"
+        :description="data.description"
+        :downloads="String(formatCompactNumber(data.downloads))"
+        :follows="String(formatCompactNumber(data.followers))"
+        :createdAt="data.published"
+        :updatedAt="data.updated"
+        :projectTypeDisplay="data.project_type"
+        :projectTypeUrl="`/${data.project_type}`"
+        :serverSide="data.server_side"
+        :clientSide="data.client_side"
+        :showUpdatedDate="false"
+        :color="data.color"
+      ></ProjectCard>
+    </div>
+    <!-- <template v-else>Data is not loaded yet</template> -->
+
+    <!-- <ProjectCard
+      id="sodium"
+      type="mod"
+      name="notSodium"
+      author="jellysquid3"
+      description="I am way too lazy"
+      iconUrl="https://github.com/worldwidepixel/tencraft/blob/main/img/9icon.png?raw=true"
+      downloads="1.51M"
+      follows="-3511"
+      createdAt="2020-12-27T19:00:00.000Z"
+      updatedAt="2021-01-01T19:00:00.000Z"
+      projectTypeDisplay="mod"
+      projectTypeUrl="/mod"
+      serverSide="required"
+      clientSide="required"
+      :showUpdatedDate="false"
+      color="1716041"
+    ></ProjectCard>
+
+    Time to use some MR branding
+    <TextLogo :animate="true"></TextLogo> -->
+
+    amogus
+
+    <Button color="highlight" @click="onPrisonSend">
+      <ScaleIcon /> Send to Prison
+    </Button>
+
+    <Badge type="admin" />
+    <Badge type="moderator" />
+    <Badge type="creator" />
+
+    <Badge type="approved" />
+    <Badge type="approved-general" />
+    <Badge type="unlisted" />
+    <Badge type="withheld" />
+    <Badge type="private" />
+    <Badge type="scheduled" />
+    <Badge type="draft" />
+    <Badge type="archived" />
+    <Badge type="rejected" />
+    <Badge type="processing" />
+
+    <Badge type="accepted" />
+    <Badge type="pending" />
+
+    <Badge type="processed" />
+    <Badge type="failed" />
+    <Badge type="returned" />
+
+    <Badge type="closed" />
+
+    <MarkdownEditor v-model="description" />
   </div>
 </template>
